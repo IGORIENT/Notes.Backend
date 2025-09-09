2d9782af9c4df1140e7c7999faccf96c83be42bc
NUR\ibagaev <ibagaev@briogroup.ru>
2025-09-09 18:08:21 +0300

    Configurated

--- Notes.Application/DependencyInjection.cs
+++ Notes.Application/DependencyInjection.cs
@@ -6,10 +6,13 @@ namespace Notes.Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplication(
-            this IServiceCollection services)
+        //метод расширение для Services
+        public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+            });
             return services;
         }
 
--- Notes.WebApi/Program.cs
+++ Notes.WebApi/Program.cs
@@ -1,22 +1,67 @@
 using Notes.Persistence;
+using Notes.Application;
+using System.Reflection;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Interfaces;
 
+//все что здесь происходит - инициализаци€ приложени€
 var builder = WebApplication.CreateBuilder(args);
+
+
+#region регистраци€ сервисов
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    cfg.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+});
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddControllers();
+
+// дл€ теста сделам так, чтобы на наш сайт могли заходить и стучатьс€ кто угодно
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+#endregion
+
+#region построение приложени€
 var app = builder.Build();
+#endregion
 
-app.MapGet("/", () => "Hello World!");
 
+#region Middleware  (эквивалент Configure)
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.MapControllers();
+#endregion
+
+#region »нициализаци€ Ѕƒ (Scope + DbInitializer)
 using (var scope = app.Services.CreateScope())
 {
     var serviceProvider = scope.ServiceProvider;
     try
     {
-        var context = serviceProvider.GetRequiredService<NotesDbContext>();
+        var context = serviceProvider.GetRequiredService<NotesDbContext>(); //¬ытаскиваю из DI реализацию интерфейса INotesDbContext
         DbInitializer.Initialize(context);
     }
-    catch (Exception ex)
+    catch (Exception exeption)
     {
 
     }
 }
+#endregion
 
+// запуск веб приложение - оно уходит в цикл обработки запросов.
 app.Run();
